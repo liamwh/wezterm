fdea1addcbdef19f5a0b24c8b02ec77a1efd137f
Liam Woodleigh <liam.woodleigh@gmail.com>
2023-08-06 21:09:54 +0200

    Open directly to wsl on windows

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,19 +1,18 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
-local config = {
-}
+local config = {}
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.color_scheme = 'ayu'
-config.font = wezterm.font 'JetBrains Mono'
+config.color_scheme = "ayu"
+config.font = wezterm.font("JetBrains Mono")
+config.harfbuzz_features = { "liga=1" }
 
 if wezterm.target_triple:find("windows") then
-  config.default_prog = {"pwsh.exe"}
+	config.default_prog = { "wsl.exe" }
 end
 
 -- finally, return the configuration to wezterm
 return config
-
