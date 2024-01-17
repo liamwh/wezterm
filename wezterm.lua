dbda8b72c0f4b77bc5ac1e737364d13135956cb1
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-01-17 20:58:24 +0100

    feat: Wezterm setup in Hyprland

--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
-local config = {}
-if wezterm.config_builder then
-    config = wezterm.config_builder()
-end
+local config = wezterm.config_builder and wezterm.config_builder() or {}
 
 config.color_scheme = "ayu"
 config.font = wezterm.font("JetBrains Mono")
