44fc0ffec03e9b0d99460590818450d71bb7e020
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-02-20 11:41:58 +0100

    fix: Don't adjust window size when changing font size

--- wezterm.lua
+++ wezterm.lua
@@ -15,6 +15,7 @@ config.keys = {
 }
 config.use_dead_keys = false
 config.enable_wayland = false
+config.adjust_window_size_when_changing_font_size = false
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 
